ca4f693814442ecbcb411609856edb4c90cdbbf7
its4zahoor <its4zahoo@gmail.com>
2021-07-15 13:56:52 +0500

    organized files into directories, added redux packages and icons css link

---
+++ src/view/index.js
@@ -0,0 +1,14 @@
+import '../styles/index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Redux-Toolkit Tutorial
+      </header>
+      {/* <TodoApp /> */}
+    </div>
+  );
+}
+
+export default App;
